874e768bfb371f12352500f59b868b819e49264a
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2023-01-07 19:41:04 +0100

    Refactor : count score

--- src/core/usecases/countScore/reducer.ts
+++ src/core/usecases/countScore/reducer.ts
@@ -1,5 +1,6 @@
 import { createReducer } from "@reduxjs/toolkit"
 import { playersShapesFormed } from "src/core/usecases/playShape/actions"
+import type { Shape } from "src/core/models/shape"
 
 export interface CountScoreState {
   player: number
@@ -15,33 +16,49 @@ export const countScore = createReducer(initial, (builder) => {
     const {
       payload: { playerShape, computerShape },
     } = action
-    if (playerShape === computerShape) {
-      state.player = 1
-      state.computer = 1
-    }
-    if (playerShape === "Paper" && computerShape === "Rock") {
-      state.player = 3
-      state.computer = 0
-    }
-    if (playerShape === "Scissors" && computerShape === "Rock") {
-      state.player = 0
-      state.computer = 3
-    }
-    if (playerShape === "Rock" && computerShape === "Paper") {
-      state.player = 0
-      state.computer = 3
-    }
-    if (playerShape === "Scissors" && computerShape === "Paper") {
-      state.player = 3
-      state.computer = 0
-    }
-    if (playerShape === "Rock" && computerShape === "Scissors") {
-      state.player = 3
-      state.computer = 0
-    }
-    if (playerShape === "Paper" && computerShape === "Scissors") {
-      state.player = 0
-      state.computer = 3
+    const roundStatus = shifumiMachineReducer({ playerShape, computerShape })
+    switch (roundStatus) {
+      case "DRAW": {
+        state.player = 1
+        state.computer = 1
+        break
+      }
+      case "WIN": {
+        state.player = 3
+        state.computer = 0
+        break
+      }
+      case "LOSE": {
+        state.player = 0
+        state.computer = 3
+        break
+      }
     }
   })
 })
+
+const shifumiMachineReducer = (round: {
+  playerShape: Shape
+  computerShape: Shape
+}) => {
+  const { playerShape, computerShape } = round
+  return shifumiMachine[playerShape][computerShape]
+}
+
+const shifumiMachine: Record<Shape, Record<Shape, "WIN" | "DRAW" | "LOSE">> = {
+  Rock: {
+    Rock: "DRAW",
+    Paper: "LOSE",
+    Scissors: "WIN",
+  },
+  Paper: {
+    Rock: "WIN",
+    Paper: "DRAW",
+    Scissors: "LOSE",
+  },
+  Scissors: {
+    Rock: "LOSE",
+    Paper: "WIN",
+    Scissors: "DRAW",
+  },
+}
