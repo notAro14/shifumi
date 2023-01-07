078e5430dde6f2dbac42c21160571d5e71b0ffa7
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2023-01-07 22:00:45 +0100

    Feat : play a round

--- src/adapters/primary/react/components/RockPaperScissors.tsx
+++ src/adapters/primary/react/components/RockPaperScissors.tsx
@@ -1,11 +1,16 @@
+import { useAppDispatch } from "src/adapters/primary/react/hooks/redux"
+import type { Shape } from "src/core/models/shape"
+import { playShape } from "src/core/usecases/playShape"
 import styles from "./RockPaperScissors.module.scss"
 
 export default function RockPaperScissors() {
+  const dispatch = useAppDispatch()
+  const makeOnClick = (shape: Shape) => () => dispatch(playShape(shape))
   return (
     <div className={styles.container}>
-      <button>Rock</button>
-      <button>Paper</button>
-      <button>Scissors</button>
+      <button onClick={makeOnClick("Rock")}>Rock</button>
+      <button onClick={makeOnClick("Paper")}>Paper</button>
+      <button onClick={makeOnClick("Scissors")}>Scissors</button>
     </div>
   )
 }
